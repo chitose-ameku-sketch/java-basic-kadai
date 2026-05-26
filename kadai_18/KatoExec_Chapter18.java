5a1173d6ae39a8d9e0a901af8f81a8847d7eb798
chitose-ameku-sketch <chitose-ameku@gr.paramix.co.jp>
2026-05-26 14:26:24 +0900

    Add files via upload

---
+++ kadai_18/KatoExec_Chapter18.java
@@ -0,0 +1,32 @@
+package kadai_18;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		KatoTaro_Chapter18 Taro = new KatoTaro_Chapter18 ();
+		
+
+		Taro.setGivenName();
+		
+		Taro.execIntroduce();
+		
+
+		KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18 ();	
+		
+		
+		Ichiro.setGivenName();
+		
+		Ichiro.execIntroduce();
+		
+
+		KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18 ();	
+		
+		
+		Hanako.setGivenName();
+		
+		Hanako.execIntroduce();
+		
+	}
+	
+	
+
+}
